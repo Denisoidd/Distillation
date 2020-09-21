dafcfdb76041d18d5b5ce013c7c1cfe17201a90f
Denisoidd <denischaynikov@phystech.edu>
2020-09-21 17:50:52 +0200

    Created config.yaml. Divide data on train and val.

--- teacher_train.py
+++ teacher_train.py
@@ -1,5 +1,6 @@
 import pathlib
 import tensorflow as tf
+from utils import load_config
 
 # download dataset with flowers 5 different classes
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
@@ -10,3 +11,37 @@ data_dir = pathlib.Path(data_dir)
 image_count = len(list(data_dir.glob('*/*.jpg')))
 if image_count:
     print("Data loaded correctly")
+
+# load config
+config = load_config("config.yaml")
+if config:
+    print("Config loaded correctly")
+
+# divide data on train and validation
+# load train part
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=config["teacher"]["train"]["val_split"],
+  subset="training",
+  seed=123,
+  image_size=(config["teacher"]["train"]["im_h"], config["teacher"]["train"]["im_w"]),
+  batch_size=config["teacher"]["train"]["b_s"])
+
+if train_ds:
+    print("Train dataset prepared successfully")
+
+# load validation part
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=config["teacher"]["train"]["val_split"],
+  subset="validation",
+  seed=123,
+  image_size=(config["teacher"]["train"]["im_h"], config["teacher"]["train"]["im_w"]),
+  batch_size=config["teacher"]["train"]["b_s"])
+
+if val_ds:
+    print("Val dataset prepared successfully")
+
+
+
+
--- utils.py
+++ utils.py
@@ -1,2 +1,10 @@
+import  yaml
+
 def check():
     print("Check is correct")
+
+def load_config(config_path):
+    with open(config_path) as file:
+        config = yaml.safe_load(file)
+
+        return config
